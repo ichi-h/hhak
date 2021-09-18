671b1d4d2837638e6482532289f91f7df7c1453c
ippee <ippee.music@gmail.com>
2021-09-18 12:19:20 +0900

    Add ReadPassphrase

---
+++ src/ReadPassphrase.hs
@@ -0,0 +1,19 @@
+module ReadPassphrase
+  ( readPassphrase
+  ) where
+
+import System.IO
+import Control.Exception
+
+readPassphrase :: IO String 
+readPassphrase = do
+  putStr "[hhak] Passphrase: "
+  hFlush stdout
+  pass <- withEcho False getLine
+  putChar '\n'
+  return pass
+
+withEcho :: Bool -> IO a -> IO a
+withEcho echo action = do
+  old <- hGetEcho stdin
+  bracket_ (hSetEcho stdin echo) (hSetEcho stdin old) action
