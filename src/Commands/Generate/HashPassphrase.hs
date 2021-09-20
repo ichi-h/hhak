75e1429fb415a6ed0e05e594c2d5764b588cfcc1
ippee <ippee.music@gmail.com>
2021-09-20 13:58:59 +0900

    Implement HashPassphrase

--- src/Commands/Generate/HashPassphrase.hs
+++ src/Commands/Generate/HashPassphrase.hs
@@ -2,7 +2,14 @@ module Commands.Generate.HashPassphrase
   ( hashPassphrase
   ) where
 
-import Args.HhakArgs (HhakArgs)
+import Args.HhakArgs (HhakArgs (title, passphrase))
+import Data.ByteString as BS ( ByteString )
+import Data.ByteString.UTF8 as BSU ( fromString )
+import Crypto.Hash ( hash, SHA3_512, Digest )
 
 hashPassphrase :: HhakArgs -> String
-hashPassphrase hhakArgs = "hashed"
+hashPassphrase hhakArgs = do
+  sha3_512 $ BSU.fromString $ title hhakArgs ++ passphrase hhakArgs
+
+sha3_512 :: BS.ByteString -> String
+sha3_512 bs = show (hash bs :: Digest SHA3_512)
