3c064d263ab6c035e65287e7777a7df92af9efa2
ippee <ippee.music@gmail.com>
2021-09-20 22:52:05 +0900

    Change hash algorithm from sha3_512 to sha3_256

--- src/Commands/Generate/HashPassphrase.hs
+++ src/Commands/Generate/HashPassphrase.hs
@@ -5,11 +5,11 @@ module Commands.Generate.HashPassphrase
 import Args.HhakArgs ( HhakArgs (title, passphrase) )
 import Data.ByteString as BS ( ByteString )
 import Data.ByteString.UTF8 as BSU ( fromString )
-import Crypto.Hash ( hash, SHA3_512, Digest )
+import Crypto.Hash ( hash, SHA3_256, Digest )
 
 hashPassphrase :: HhakArgs -> String
 hashPassphrase hhakArgs = do
-  sha3_512 $ BSU.fromString $ title hhakArgs ++ passphrase hhakArgs
+  sha3_256 $ BSU.fromString $ title hhakArgs ++ passphrase hhakArgs
 
-sha3_512 :: BS.ByteString -> String
-sha3_512 bs = show (hash bs :: Digest SHA3_512)
+sha3_256 :: BS.ByteString -> String
+sha3_256 bs = show (hash bs :: Digest SHA3_256)
