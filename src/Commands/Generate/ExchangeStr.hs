1678e299a22103c30dcd5eb5856d2fec2487d4cc
ippee <ippee.music@gmail.com>
2021-09-20 23:21:07 +0900

    Add workflow of exchangeStr

--- src/Commands/Generate/ExchangeStr.hs
+++ src/Commands/Generate/ExchangeStr.hs
@@ -2,7 +2,18 @@ module Commands.Generate.ExchangeStr
   ( exchangeStr
   ) where
 
-import Args.HhakArgs ( HhakArgs )
+import Args.HhakArgs ( HhakArgs (options), Options (sym) )
 
 exchangeStr :: HhakArgs -> String -> String
-exchangeStr hhakArgs hashed = "password"
+exchangeStr hhakArgs hashed = do
+  let symChars = genSymChars $ sym $ options hhakArgs
+  randomReplace symChars $ removeSyms hashed
+
+genSymChars :: String -> String
+genSymChars symOpt = "foo"
+
+removeSyms :: String -> String
+removeSyms password = "bar"
+
+randomReplace :: String -> String -> String
+randomReplace symChars password = "baz"
