6785652b7a9bc01899a07fa94220b277d93d9bd7
ippee <ippee.music@gmail.com>
2021-09-20 22:17:39 +0900

    Add genSalt function

--- src/Commands/Generate/HashByBCrypt.hs
+++ src/Commands/Generate/HashByBCrypt.hs
@@ -2,7 +2,20 @@ module Commands.Generate.HashByBCrypt
   ( hashByBCrypt
   ) where
 
-import Args.HhakArgs ( HhakArgs )
+import Args.HhakArgs ( HhakArgs, Algorithm, algoToStr )
+import Util.StringOperation ( joinStr )
+import Data.ByteString ( ByteString )
+import Data.ByteString.UTF8 as BSU ( fromString )
+import System.Random ( mkStdGen )
+import System.Random.Stateful ( UniformRange(uniformRM), StatefulGen, runStateGen_ )
+import Control.Monad ( replicateM )
 
 hashByBCrypt :: HhakArgs -> String -> String
 hashByBCrypt hhakArgs hashedPass = "hashed"
+
+genRealSalt :: Int -> String
+genRealSalt seed = do
+  let c = ['a'..'z'] ++ ['A'..'Z'] ++ ['.', '/']
+  let rdCharM = uniformRM (0, Prelude.length c - 1) :: StatefulGen g m => g -> m Int
+  let pureGen = mkStdGen seed
+  Prelude.map (c !!) $ runStateGen_ pureGen (replicateM 22 . rdCharM)
