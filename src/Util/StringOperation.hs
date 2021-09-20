fbb77a9055b6ba13b6e49c3f330f6482392a7469
ippee <ippee.music@gmail.com>
2021-09-21 00:00:51 +0900

    Add include function

--- src/Util/StringOperation.hs
+++ src/Util/StringOperation.hs
@@ -1,11 +1,22 @@
 module Util.StringOperation
   ( startWith
+  , include
   , joinStr
   ) where
 
 startWith :: String -> String -> Bool
 startWith _ [] = False
 startWith target searchStr = take (length searchStr) target == searchStr
+
+include :: String -> String -> Bool
+include _ [] = False
+include target searchStr =
+  if length searchStr < length target then
+    False
+  else if take (length target) searchStr == target then
+    True
+  else
+    include target (tail searchStr)
 
 joinStr :: String -> [String] -> String
 joinStr _ [] = ""
