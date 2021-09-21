1ed1d001ab2f4813ebfcc3618751df11d696d090
ippee <ippee.music@gmail.com>
2021-09-21 16:26:19 +0900

    Add GenPasswordTest

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,7 +2,9 @@ module Main where
 
 import Test.Hspec ( hspec )
 import Args.HhakArgsGeneratorTest ( specGenHhakArgs )
+import Commands.Generate.GenPasswordTest ( specGenPassword )
 
 main :: IO ()
 main = hspec $ do
   specGenHhakArgs
+  specGenPassword
