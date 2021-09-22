ebd6d696740da82011576ef1dc8db25f7368089b
ippee <ippee.music@gmail.com>
2021-09-22 11:18:34 +0900

    Implement afterGen function

--- src/Commands/Generate/GenCmd.hs
+++ src/Commands/Generate/GenCmd.hs
@@ -3,16 +3,57 @@ module Commands.Generate.GenCmd
   , afterGen
   ) where
 
-import Args.HhakArgs ( HhakArgs (passphrase) )
+import Args.HhakArgs ( HhakArgs (passphrase, options), Options (display) )
 import Commands.Generate.HashPassphrase ( hashPassphrase )
 import Commands.Generate.HashByBCrypt ( hashByBCrypt )
 import Commands.Generate.ReplaceChars ( replaceChars )
-import System.Process (readProcess)
+import Util.StringOperation ( include )
 
-genCmd :: HhakArgs -> String
-genCmd hhakArgs = replaceChars hhakArgs $ hashByBCrypt hhakArgs $ hashPassphrase hhakArgs
+import System.Exit ( exitWith, ExitCode (ExitFailure) )
+import System.Process ( callCommand )
+import System.Info ( os )
+import Control.Exception ( catch, SomeException (SomeException) )
 
-afterGen :: String -> IO ()
+data GenResult = GenResult { password :: String
+                           , isDisplay :: Bool
+                           }
+
+genCmd :: HhakArgs -> GenResult
+genCmd hhakArgs = do
+  let password = replaceChars hhakArgs $ hashByBCrypt hhakArgs $ hashPassphrase hhakArgs
+  GenResult { password = password
+            , isDisplay = display $ options hhakArgs
+            }
+
+afterGen :: GenResult -> IO ()
 afterGen result = do
-  _ <- readProcess "echo foo" [] []
-  putStrLn result
+  let pass = foldl (\acc c -> if c == '"' then acc ++ "\\\"" else acc ++ [c]) [] (password result)
+
+  catch (do
+    case os of
+      "mingw32" -> callCommand $ "echo \"" ++ pass ++ "\" | clip.exe"
+      "linux"   -> callCommand $ "echo \"" ++ pass ++ "\" | xclip -selection c"
+      "darwin"  -> callCommand $ "echo \"" ++ pass ++ "\" | pbcopy"
+      _ -> do
+        putStrLn "Sorry, your platform is not yet supported."
+        exitWith (ExitFailure 1)
+    ) $ \(SomeException e) -> do
+      putStrLn $ hidePass [] pass $ show e
+      exitWith (ExitFailure 1)
+
+  putStrLn "Password was copied to clipboard!"
+  if isDisplay result then
+    putStrLn $ "Password: " ++ password result
+  else
+    putStr ""
+
+hidePass :: String -> String -> String -> String
+hidePass _ [] searchStr = searchStr
+hidePass acc _ [] = acc
+hidePass acc (x:xs) searchStr = do
+  let len = length (x:xs)
+  let trimed = take len searchStr
+  if trimed == (x:xs) then
+    acc ++ replicate 4 '*' ++ drop len searchStr
+  else
+    hidePass (acc ++ [head searchStr]) (x:xs) (tail searchStr)
