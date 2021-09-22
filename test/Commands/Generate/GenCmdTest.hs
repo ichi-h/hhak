c33ecb94f71ae6768fd8cefb5120586a5c0836a0
ippee <ippee.music@gmail.com>
2021-09-22 12:34:25 +0900

    Fix type to be compared

--- test/Commands/Generate/GenCmdTest.hs
+++ test/Commands/Generate/GenCmdTest.hs
@@ -3,7 +3,7 @@ module Commands.Generate.GenCmdTest
   ) where
 
 import Test.Hspec ( describe, it, shouldBe, shouldNotBe, Spec )
-import Commands.Generate.GenCmd ( genCmd )
+import Commands.Generate.GenCmd ( genCmd, GenResult (..) )
 import Args.HhakArgs ( HhakArgs (..), Options (..), Algorithm (..), defaultOptions, defaultHhakArgs )
 import Util.StringOperation ( include )
 import Data.Either (fromRight)
@@ -15,21 +15,22 @@ specGenCmd = do
   describe "Normal behavior" $ do
     it "change passphrase" $ do
       let result = genCmd defaultHhakArgs { passphrase = "sample" }
-      result `shouldNotBe` defaultPass
+      show result `shouldNotBe` show defaultPass
 
     it "change title" $ do
       let result = genCmd defaultHhakArgs { title = "GitHub" }
-      result `shouldNotBe` defaultPass
+      show result `shouldNotBe` show defaultPass
 
     it "change password length" $ do
-      let result = genCmd defaultHhakArgs { options = defaultOptions { len = 10 } }
+      let result = password $ genCmd defaultHhakArgs { options = defaultOptions { len = 10 } }
       length result `shouldBe` 10
 
     it "change cost" $ do
       let result = genCmd defaultHhakArgs { options = defaultOptions { cost = 5 } }
-      result `shouldNotBe` defaultPass
+      show result `shouldNotBe` show defaultPass
 
     it "generate password without symbols" $ do
       let result = filter (\c -> not $ include [c] (['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9']))
+                   $ password
                    $ genCmd defaultHhakArgs { options = defaultOptions { sym = "" } }
       length result `shouldBe` 0
