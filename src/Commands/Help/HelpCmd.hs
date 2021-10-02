2d871a47df94a45e13095402eaf81b2550890a56
ippee <ippee.music@gmail.com>
2021-10-02 23:45:23 +0900

    Fix help text

--- src/Commands/Help/HelpCmd.hs
+++ src/Commands/Help/HelpCmd.hs
@@ -6,24 +6,26 @@ module Commands.Help.HelpCmd
 helpCmd :: String
 helpCmd =
     "\
-    \Usage: dhak [-h, --help] [-v, --version] <title> (<preset>) [options]\n\
+    \Usage: hhak [-h, --help] [-v, --version] <title> (<preset>) [options]\n\
     \\n\
     \Command:\n\
     \    -h, --help\n\
-    \        Display the help of Dhak.\n\
+    \        Display the help of Hhak.\n\
     \    -v, --version\n\
-    \        Display the version of Dhak.\n\
+    \        Display the version of Hhak.\n\
     \Title:\n\
     \    The name of the service.\n\
     \Preset:\n\
-    \    The pre-prepared setting for password generation (you can add a preset to ~/.dhakrc).\n\
+    \    This feature has not been implemented yet.\n\
+    \    The pre-prepared setting for password generation (you can add a preset to ~/.hhakrc).\n\
     \Options:\n\
     \    Specify additional functions as needed.\n\
-    \    Dhak will use the optional settings in preference to the preset ones.\n\
-    \    The following values of the options mean the default values in this app. If you omit a option, Dhak will use them.\n\
+    \    Hhak will use the optional settings in preference to the preset ones.\n\
+    \    The following values of the options mean the default values in this app. If you omit a option, Hhak will use them.\n\
     \    -d, --display\n\
     \        Display the password in the terminal.\n\
     \    -f, --force (deprecated)\n\
+    \        This feature has not been implemented yet.\n\
     \        Forcibly generate a password which may be insecure, such as a password whose length is less than 12 or a password which has only lower-case.\n\
     \    --len=20\n\
     \        Set a password length. If it is less than 12, you cannot generate the password basically.\n\
@@ -32,9 +34,7 @@ helpCmd =
     \    --algo=2b\n\
     \        Set an algorithm of BCrypt. You can use \"2\", \"2a\", \"2y\" and \"2b\".\n\
     \    --cost=10\n\
-    \        Set a cost of BCrypt. It must be between 4 and 31. The actual rounds of stretching are 2^n.\n\
-    \\n\
-    \For more details, see https://ippee.github.io/dhak_docs/"
+    \        Set a cost of BCrypt. It must be between 4 and 31. The actual rounds of stretching are 2^n."
 
 afterHelp :: String -> IO ()
 afterHelp = putStrLn
